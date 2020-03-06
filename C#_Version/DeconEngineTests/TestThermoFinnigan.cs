a012a4b516160c597e4890314e38056088ce632d
alchemistmatt <matthew.monroe@pnnl.gov>
2020-03-06 15:20:31 -0800

    Update unit test

--- C#_Version/DeconEngineTests/TestThermoFinnigan.cs
+++ C#_Version/DeconEngineTests/TestThermoFinnigan.cs
@@ -1,4 +1,4 @@
-﻿#if Enable_Obsolete
+﻿using System;
 using NUnit.Framework;
 
 namespace DeconEngineTests
@@ -6,18 +6,37 @@ namespace DeconEngineTests
     public class TestThermoFinnigan
     {
         [Test]
-        public void TestFinnigan()
+        public void TestThermoRaw()
         {
-            var finReader = new Engine.Readers.FinniganRawData();
-            int scan = 100;
-            finReader.Open(@"E:\DeconMSn_data\QC_Shew_13_07-500ng_4a_01Sept14_Falcon_14-07-07.raw");
-            double temp = finReader.GetAGCAccumulationTime(scan);
-            System.Console.WriteLine(temp);
-            temp = finReader.GetMonoChargeFromHeader(scan);
-            System.Console.WriteLine(temp);
-            temp = finReader.GetMonoMZFromHeader(scan);
-            System.Console.WriteLine(temp);
+
+            // Note: We can only read Thermo .raw file data when compiled as x64
+
+            var thermoReader = new Engine.Readers.ThermoRawData();
+
+            var scanNum = 5000;
+            thermoReader.Open(@"\\proto-2\UnitTest_Files\DeconTools_TestFiles\QC_Shew_08_04-pt5-2_11Jan09_Sphinx_08-11-18.RAW");
+
+            var elutionTime = thermoReader.GetScanTime(scanNum);
+            var msLevel = thermoReader.GetMSLevel(scanNum);
+            var parentIonMz = thermoReader.GetParentMz(scanNum);
+            var agcTime = thermoReader.GetAGCAccumulationTime(scanNum);
+            var chargeState = thermoReader.GetMonoChargeFromHeader(scanNum);
+            var monoisotopicMz = thermoReader.GetMonoMZFromHeader(scanNum);
+
+            Console.WriteLine("{0,-15} {1}", "Scan number:", scanNum);
+            Console.WriteLine("{0,-15} {1:F2}", "Elution time:", elutionTime);
+            Console.WriteLine("{0,-15} {1:F2}", "Parent Ion m/z:", parentIonMz);
+            Console.WriteLine("{0,-15} {1}", "Charge:", chargeState);
+            Console.WriteLine("{0,-15} {1:F3}", "Ion injection time (ms):", agcTime);
+            Console.WriteLine("{0,-15} {1:F3}", "Monoisotopic m/z:", monoisotopicMz);
+
+            Assert.AreEqual(27.8, elutionTime, 0.01);
+            Assert.AreEqual(694.38, parentIonMz, 0.01);
+            Assert.AreEqual(2, chargeState);
+            Assert.AreEqual(7.523, agcTime, 0.01);
+            Assert.AreEqual(694.379, monoisotopicMz, 0.01);
+
+
         }
     }
 }
-#endif
